c0f90e3d4973c006419e6dde2dec9f60458da89d
sarathchan <sarathchan20@gmail.com>
2023-03-14 12:40:03 +0530

    chan

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apie8c58eaa": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifyappf06a6146": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify'
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const myAPI = 'apie8c58eaa'
+const path = '/patients'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [patients, setpatients] = useState([])
+
+  //Function to fetch from our backend and update patients array
+  function getpatient(e) {
+    let patientId = e.input
+    API.get(myAPI, path + "/" + patientId)
+       .then(response => {
+         console.log(response)
+         let newpatients = [...patients]
+         newpatients.push(response)
+         setpatients(newpatients)
+
+       })
+       .catch(error => {
+         console.log(error)
+       })
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+      <div>
+          <input placeholder="patient id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getpatient({input})}>Get patient From Backend</button>
+
+      <h2 style={{visibility: patients.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       patients.map((thispatient, index) => {
+         return (
+        <div key={thispatient.patientId}>
+          <span><b>patientId:</b> {thispatient.patientId} - <b>patientName</b>: {thispatient.patientName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
